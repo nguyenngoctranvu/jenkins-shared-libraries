d641409f6eca57b3c599588b5932c4fadb5f2a64
vu_nguyen <vu.nguyen@sjmx.global>
2019-11-27 17:07:45 +0700

    update oaclient

--- vars/k8sUpgradeOaClient.groovy
+++ vars/k8sUpgradeOaClient.groovy
@@ -1,6 +1,6 @@
 def call(project, namespace, clientUrl) {
   sh """helm upgrade ${project} helm/oa-client \
   -i -n ${namespace} --set image.tag=${currentBuild.displayName} \
-  --set ingress.host=${clientUrl} --dry-run --debug
+  --set ingress.host=${clientUrl}
   """
 }
