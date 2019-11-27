3928ac08a9bcea4bc0e7a1e3e0760c06126fd946
vu_nguyen <vu.nguyen@sjmx.global>
2019-11-27 10:25:38 +0700

    add deploy reportingjob

---
+++ vars/k8sUpgradeReport.groovy
@@ -0,0 +1,21 @@
+def call(project, namespace, apiurl, bucket) {
+  withCredentials([
+    string(
+      credentialsId: 'oaApi.authToken',
+      variable: 'API_AUTH_TOKEN'
+    ),
+    string(
+      credentialsId: 'mongo.Url',
+      variable: 'MONGO_URL'
+    )
+  ]) {
+    sh """helm upgrade ${project} helm/mc-daily-job \
+    -i -n ${namespace} --set image.tag=${currentBuild.displayName} \
+    --set mongo.Url=${MONGO_URL} \
+    --set oaApi.authToken=${API_AUTH_TOKEN} \
+    --set schedule.Hour=23,schedule.Minute=35 \
+    --set oaApi.Url=${apiurl} \
+    --set bucket.Name=${bucket}
+    """
+  }
+}
