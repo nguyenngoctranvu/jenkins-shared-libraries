e43e6a2509c2eb7d02d8f2e928ea51f4cdf10704
vu_nguyen <vu.nguyen@sjmx.global>
2019-11-27 17:32:11 +0700

    add deploy oaserver

---
+++ vars/k8sUpgradeOaServer.groovy
@@ -0,0 +1,16 @@
+def call(project, namespace,apiUrl, dbUrl) {
+  withCredentials([usernamePassword(
+    credentialsId: "mysql.oa",
+    usernameVariable: "USER",
+    passwordVariable: "PASS"
+  )]) {
+        sh """helm upgrade ${project} helm/oa-server \
+        -i -n ${namespace} \
+        --set image.tag=${currentBuild.displayName} \
+        --set ingress.host=${apiUrl} \
+        --set db.address=${dbUrl} \
+        --set db.user=${USER} \
+        --set db.password=${PASS} --dry-run --debug
+        """
+    }
+}
