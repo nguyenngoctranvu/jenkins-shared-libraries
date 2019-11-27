a143e8cb46fce3d8d7cbd2918874e43be9d7b49d
vu_nguyen <vu.nguyen@sjmx.global>
2019-11-27 11:05:15 +0700

    update report

--- vars/k8sUpgradeReport.groovy
+++ vars/k8sUpgradeReport.groovy
@@ -7,6 +7,14 @@ def call(project, namespace, apiurl, bucket) {
     string(
       credentialsId: 'mongo.Url',
       variable: 'MONGO_URL'
+    ),
+    string(
+      credentialsId: 'S3.Key',
+      variable: 'S3_KEY'
+    ),
+    string(
+      credentialsId: 'S3.Secret',
+      variable: 'S3_SECRET'
     )
   ]) {
     sh """helm upgrade ${project} helm/reportingjob \
@@ -14,7 +22,9 @@ def call(project, namespace, apiurl, bucket) {
     --set mongo.Url=${MONGO_URL} \
     --set oaApi.authToken=${API_AUTH_TOKEN} \
     --set oaApi.Url=${apiurl} \
-    --set bucket.Name=${bucket} --dry-run --debug
+    --set bucket.Name=${bucket} \
+    --set bucket.Key=${S3_KEY} \
+    --set bucket.Secret=${S3_SECRET} --dry-run --debug
     """
   }
 }
