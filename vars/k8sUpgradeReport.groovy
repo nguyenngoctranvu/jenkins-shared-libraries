f43863cca1852fd2cf0ba3bcd9f1c4e7771f5ed1
vu_nguyen <vu.nguyen@sjmx.global>
2019-11-27 10:49:18 +0700

    update report

--- vars/k8sUpgradeReport.groovy
+++ vars/k8sUpgradeReport.groovy
@@ -13,7 +13,6 @@ def call(project, namespace, apiurl, bucket) {
     -i -n ${namespace} --set image.tag=${currentBuild.displayName} \
     --set mongo.Url=${MONGO_URL} \
     --set oaApi.authToken=${API_AUTH_TOKEN} \
-    --set schedule.Hour=23,schedule.Minute=35 \
     --set oaApi.Url=${apiurl} \
     --set bucket.Name=${bucket} --dry-run --debug
     """
