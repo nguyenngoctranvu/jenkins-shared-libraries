35605181401b4659354a971a54f9dba499d90e4c
vu_nguyen <vu.nguyen@sjmx.global>
2020-01-13 14:02:56 +0700

    add mc frontend

---
+++ vars/k8sUpgradeMC.groovy
@@ -0,0 +1,34 @@
+def call(project, namespace, feUrl) {
+  withCredentials([
+    usernamePassword(
+      credentialsId: "SJMBT_GATEWAY_URL",
+      usernameVariable: "SJMBT_GATEWAY_URL",
+      passwordVariable: ""
+    ),
+    usernamePassword(
+      credentialsId: "COGNITO_WEB_CLIENT_ID",
+      usernameVariable: "",
+      passwordVariable: "COGNITO_WEB_CLIENT_ID"
+    ),
+    usernamePassword(
+      credentialsId: "COGNITO_USER_POOL_ID",
+      usernameVariable: "COGNITO_USER_POOL_ID",
+      passwordVariable: ""
+    ),
+    usernamePassword(
+      credentialsId: "COGNITO_REGION",
+      usernameVariable: "COGNITO_REGION",
+      passwordVariable: ""
+    )
+  ]) {
+        sh """helm upgrade ${project} helm/mc \
+        -i -n ${namespace} \
+        --set image.tag=${currentBuild.displayName} \
+        --set ingress.host=${feUrl} \
+        --set gateway.url=${SJMBT_GATEWAY_URL} \
+        --set cognito.web_client_id=${COGNITO_WEB_CLIENT_ID} \
+        --set cognito.user_pool_id=${COGNITO_USER_POOL_ID} \
+        --set cognito.region=${COGNITO_REGION} \
+        """
+      }
+}
