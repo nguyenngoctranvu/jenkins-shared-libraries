4def441e686d70a34f7e0369b1d38d73f85423ea
vu_nguyen <vu.nguyen@sjmx.global>
2020-01-20 13:49:07 +0700

    update mc

--- vars/k8sUpgradeMC.groovy
+++ vars/k8sUpgradeMC.groovy
@@ -8,6 +8,7 @@ def call() {
   ]) {
     sh """helm upgrade ${JOB_BASE_NAME}-${ENV} helm/${JOB_BASE_NAME} \
     -i -n mc-${ENV} \
+    --set ingress.host=${MC_URL} \
     --set image.tag=${currentBuild.displayName} \
     --set cognito.region=${COGNITO_REGION} \
     --set cognito.user_pool_id=${COGNITO_USER_POOL_ID} \
