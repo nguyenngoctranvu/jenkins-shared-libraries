51a07135bdd6f26c3c1b8463abf667aca7a46149
vu_nguyen <vu.nguyen@sjmx.global>
2020-02-07 15:37:28 +0700

    correct upgrade mc

--- vars/k8sUpgradeMC.groovy
+++ vars/k8sUpgradeMC.groovy
@@ -1,6 +1,7 @@
 def call() {
-    sh """helm upgrade ${JOB_BASE_NAME}-${ENV} helm/${JOB_BASE_NAME} \
-    -i -n mc-${ENV} \
-    --set image.tag=${currentBuild.displayName} \
-    """
+  sh """helm upgrade ${JOB_BASE_NAME}-${ENV} helm/${JOB_BASE_NAME} \
+  -i -n mc-${ENV} \
+  --set ingress.host=${MC_URL} \
+  --set image.tag=${currentBuild.displayName} \
+  """
 }
