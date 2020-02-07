fcf4e88f4716ad66fd3134b8c21f905e8cf663ce
vu_nguyen <vu.nguyen@sjmx.global>
2020-02-07 11:06:01 +0700

    update upgrade mc

--- vars/k8sUpgradeMC.groovy
+++ vars/k8sUpgradeMC.groovy
@@ -4,6 +4,11 @@ def call() {
       credentialsId: "COGNITO_WEB_CLIENT_ID",
       usernameVariable: "BLANK",
       passwordVariable: "COGNITO_WEB_CLIENT_ID"
+    ),
+    usernamePassword(
+      credentialsId: "SJMBT_EMAIL_AUTH_PASS",
+      usernameVariable: "BLANK",
+      passwordVariable: "SJMBT_EMAIL_AUTH_PASS"
     )
   ]) {
     sh """helm upgrade ${JOB_BASE_NAME}-${ENV} helm/${JOB_BASE_NAME} \
@@ -15,7 +20,10 @@ def call() {
     --set gateway.url=${GW_URL} \
     --set cognito.region=${COGNITO_REGION} \
     --set cognito.user_pool_id=${COGNITO_USER_POOL_ID} \
-    --set cognito.web_client_id=${COGNITO_WEB_CLIENT_ID}
+    --set cognito.web_client_id=${COGNITO_WEB_CLIENT_ID} \
+    --set sjmbt.admin_email_addr=${SJMBT_ADMIN_EMAIL_ADDR} \
+    --set sjmbt.email_auth_user=${SJMBT_EMAIL_AUTH_USER} \
+    --set sjmbt.email_auth_pass=${SJMBT_EMAIL_AUTH_PASS} 
     """
   }
 }
