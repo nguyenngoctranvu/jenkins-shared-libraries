adfb33eb21bce73c040692fbaeda98e2672b511a
vu_nguyen <vu.nguyen@sjmx.global>
2020-02-14 10:50:08 +0700

    correct upgrade

--- vars/k8sUpgradeMC.groovy
+++ vars/k8sUpgradeMC.groovy
@@ -1,21 +1,7 @@
 def call() {
-  withCredentials([
-    usernamePassword(
-      credentialsId: "LOG_ACCESS_KEY",
-      usernameVariable: "LOG_ACCESS_KEY",
-      passwordVariable: "LOG_SECRET_ACCESS_KEY"
-    )
-  ]) {
-    sh """helm upgrade ${JOB_BASE_NAME}-${ENV} helm/${JOB_BASE_NAME} \
-    -i -n mc-${ENV} \
-    --set ingress.host=${MC_URL} \
-    --set image.tag=${currentBuild.displayName} \
-    --set log.group_name=${LOG_GROUP_NAME} \
-    --set log.stream=${LOG_STREAM} \
-    --set log.region=${LOG_REGION} \
-    --set log.access_key=${LOG_ACCESS_KEY} \
-    --set log.secret_access_key=${LOG_SECRET_ACCESS_KEY} \
-    --set log.is_local=${LOG_IS_LOCAL} 
-    """
-  }
+  sh """helm upgrade ${JOB_BASE_NAME}-${ENV} helm/${JOB_BASE_NAME} \
+  -i -n mc-${ENV} \
+  --set ingress.host=${MC_URL} \
+  --set image.tag=${currentBuild.displayName} 
+  """
 }
