b25a5b483b11e35a2aac895fdeccd6c558d435b0
vu_nguyen <vu.nguyen@sjmx.global>
2020-02-21 14:12:18 +0700

    correct ob

--- vars/k8sUpgradeOB.groovy
+++ vars/k8sUpgradeOB.groovy
@@ -8,7 +8,7 @@ def call() {
   ]) {
     sh """helm upgrade ${JOB_BASE_NAME} helm/${JOB_BASE_NAME} \
     -i -n mc-${ENV} \
-    --set ingress.host=${OB_URL}
+    --set ingress.host=${OB_URL} \
     --set image.tag=${currentBuild.displayName} \
     --set cognito.region=${COGNITO_REGION} \
     --set cognito.user_pool_id=${COGNITO_USER_POOL_ID} \
