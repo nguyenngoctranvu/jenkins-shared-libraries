b6bfaa967b8506abd58f9caa60c95103e2ab856f
vu_nguyen <vu.nguyen@sjmx.global>
2020-02-21 14:00:17 +0700

    add script OB

---
+++ vars/k8sUpgradeOB.groovy
@@ -0,0 +1,18 @@
+def call() {
+  withCredentials([
+    usernamePassword(
+      credentialsId: "COGNITO_WEB_CLIENT_ID",
+      usernameVariable: "BLANK",
+      passwordVariable: "COGNITO_WEB_CLIENT_ID"
+    )
+  ]) {
+    sh """helm upgrade ${JOB_BASE_NAME} helm/${JOB_BASE_NAME} \
+    -i -n mc-${ENV} \
+    --set ingress.host=${OB_URL}
+    --set image.tag=${currentBuild.displayName} \
+    --set cognito.region=${COGNITO_REGION} \
+    --set cognito.user_pool_id=${COGNITO_USER_POOL_ID} \
+    --set cognito.web_client_id=${COGNITO_WEB_CLIENT_ID} 
+    """
+  }
+}
