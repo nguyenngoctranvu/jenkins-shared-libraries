7f54da62db01cc6a93f6306711cd9255be34af04
vu_nguyen <vu.nguyen@sjmx.global>
2020-03-02 15:49:13 +0700

    upgrade mc ob

--- vars/k8sUpgradeMC.groovy
+++ vars/k8sUpgradeMC.groovy
@@ -1,7 +1,7 @@
 def call() {
   sh """helm upgrade ${JOB_BASE_NAME}-${ENV} helm/${JOB_BASE_NAME} \
   -i -n mc-${ENV} \
-  --set ingress.host=${MC_URL} \
+  --set ingress.host=${MISSION_CONTROL_URL} \
   --set image.tag=${currentBuild.displayName} 
   """
 }
--- vars/k8sUpgradeOB.groovy
+++ vars/k8sUpgradeOB.groovy
@@ -6,6 +6,10 @@ def call() {
       passwordVariable: "COGNITO_WEB_CLIENT_ID"
     )
   ]) {
+    sh """kubectl create secret generic ${JOB_BASE_NAME}-${ENV} \
+    --from-literal=COGNITO_WEB_CLIENT_ID=${COGNITO_WEB_CLIENT_ID} \
+    --dry-run -o yaml > helm/${JOB_BASE_NAME}/templates/${JOB_BASE_NAME}-${ENV}.yml
+    """
     sh """helm upgrade ${JOB_BASE_NAME}-${ENV} helm/${JOB_BASE_NAME} \
     -i -n mc-${ENV} \
     --set ingress.host.gw=${GW_URL} \
@@ -13,7 +17,7 @@ def call() {
     --set image.tag=${currentBuild.displayName} \
     --set cognito.region=${COGNITO_REGION} \
     --set cognito.user_pool_id=${COGNITO_USER_POOL_ID} \
-    --set cognito.web_client_id=${COGNITO_WEB_CLIENT_ID} 
+    --set mission_control.url=${MISSION_CONTROL_URL} 
     """
   }
 }
